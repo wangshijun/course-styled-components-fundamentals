af0b1053908cc25373b89295a282b2802ae72bc7
wangshijun2010 <wangshijun2010@gmail.com>
2017-09-26 07:54:31 +0800

    basic skeleton for demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const contacts = [
+  { name: 'Lucy', gender: 'male', age: 30, introduction: 'developer' },
+  { name: 'Lily', gender: 'female', age: 28, introduction: 'painter' },
+  { name: 'Lilei', gender: 'male', age: 35, introduction: 'English teacher' },
+  { name: 'John', gender: 'female', age: 24, introduction: 'web designer' },
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        {contacts.map(contact => (
+          <div className={`contact contact--${contact.gender}`}>
+            <h2 className="contact__name">{contact.name}</h2>
+            <p className="contact__intro">
+              {contact.name} is a <strong>{contact.age} years</strong> old <strong>{contact.introduction}</strong>
+            </p>
+          </div>
+        ))}
       </div>
     );
   }
