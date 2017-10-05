46c1d6565be7f14f74b3ca00f420defc8013d777
wangshijun2010 <wangshijun2010@gmail.com>
2017-10-06 06:02:17 +0800

    (format): code

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             <h2 className="contact__name">{contact.name}</h2>
             <p className="contact__intro">
               {contact.name} is a <strong>{contact.age} years</strong> old
-              <strong>{contact.introduction}</strong>
+              <strong> {contact.introduction}</strong>
             </p>
           </div>
         ))}
