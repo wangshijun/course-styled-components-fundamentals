7376f3d1623c29c0d6572d343328479fbcdb24f2
wangshijun2010 <wangshijun2010@gmail.com>
2017-10-06 07:35:19 +0800

    (refactor): split components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { injectGlobal } from 'styled-components';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
