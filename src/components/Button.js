85c56ec1b05c70defbb9f0e4aa3d1e8fa7da1a44
wangshijun2010 <wangshijun2010@gmail.com>
2017-10-07 16:41:42 +0800

    lesson 06: using keyframes for animation

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,9 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(359deg); }
+`;
 
 const Button = styled.div`
   display: inline-block;
@@ -8,6 +13,7 @@ const Button = styled.div`
   margin: 15px;
   color: white;
   background: ${props => props.theme.primary};
+  animation: ${spin} 1s linear infinite;
 `;
 
 export default Button;
